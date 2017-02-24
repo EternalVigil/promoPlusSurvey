44393f2da03f4b1ea3bbf607e0be67a7cc7fed30
Brandon <theeternalvigil@gmail.com>
2017-02-23 17:52:25 -0800

    file structure created, skeleton established

---
+++ server.js
@@ -0,0 +1,26 @@
+// PromoPlusSurvey PoC - server.js
+
+var express = require ("express");
+var bodyParser = require ("body-parser");
+var logger = require ("morgan");
+var mongoose = require ("mongoose");
+
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:"application/vnd.api+json"}));
+
+app.use(express.static("./public"));
+
+app.get("/", function(request, response){
+	response.sendFile(__dirname + "/public.index.html");
+});
+
+app.listen(PORT, function(){
+	console.log("App is listening on port " + PORT);
+});
